96a90c4fcd401eb1d7a61b943bcb521cbe1bde94
ltvc <38085145+ltvc@users.noreply.github.com>
2018-05-21 18:22:10 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
-var a=10, h=20;
+var a=prompt ('Skrypt oblicza pole trojkata, podaj dlugosc podstawy:');
+var h=prompt ('podaj wysokosc:');
 var triangleArea=a*h/2;
-alert(triangleArea);
+alert('pole trojkata wynosi= '+triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
